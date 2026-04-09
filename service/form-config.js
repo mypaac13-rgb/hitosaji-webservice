1752ae0d804029989ff1110e0567284a313f4221
mypaac13-rgb <mypaac13@gmail.com>
2026-04-09 14:43:20 +0900

    Prioritize LINE on service application page

--- service/form-config.js
+++ service/form-config.js
@@ -1,6 +1,7 @@
 window.SERVICE_FORM_CONFIG = {
   endpoint: '',
   ownerEmail: 'mypaac13@gmail.com',
+  lineUrl: 'https://lin.ee/PRI24cC',
   subjectPrefix: '静かな1ページサイト制作',
   successMessage: 'お申し込みを受け付けました。内容を確認して、2日以内を目安にご連絡します。'
 };
